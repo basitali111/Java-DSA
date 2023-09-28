e6aab592447dc84a4ea908276cb2ca256e32bd7b
basitali111 <allahbadshah148@gmail.com>
2023-09-29 02:05:48 +0500

    Add total subarray code

---
+++ Arrays/totalsubarrays.java
@@ -0,0 +1,25 @@
+class ArrayCC {
+    public static void printSubarray(int numbers[]) {
+        int total = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                System.out.print("[");
+                for (int k = i; k <= j; k++) {
+                    System.out.print(numbers[k]);
+                    if (k < j) {
+                        System.out.print(", ");
+                    }
+                }
+                System.out.print("]");
+                total++;
+                System.out.println();
+            }
+        }
+        System.out.println("Total subarrays: " + total);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 3, 4, 5};
+        printSubarray(numbers);
+    }
+}
